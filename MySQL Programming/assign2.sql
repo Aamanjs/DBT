01d12db3b6ac3a671cb7a522c7762b3d5f40b25b
Aaman Sayyad <86286688+Aamanjs@users.noreply.github.com>
2025-09-25 09:54:24 +0530

    Update assign2.sql

--- MySQL Programming/assign2.sql
+++ MySQL Programming/assign2.sql
@@ -96,3 +96,83 @@ select concat("The course name of ", s_name," is: ", pcourse) as Course_details;
 end//
 
 call q5("Alice");
+
+
+
+-- Problem Statements using SELECT INTO with OUT Parameter
+-- 1.	Write a procedure that retrieves the name of the topper student (highest marks) into an OUT parameter using SELECT INTO and display the retrieved name.
+-- 2.	Write a procedure that retrieves the lowest marks scored by any student into an OUT parameter using SELECT INTO and display the retrieved marks.
+-- 3.	Write a procedure that retrieves the total number of students into an OUT parameter using SELECT INTO and display the retrieved count.
+-- 4.	Write a procedure that retrieves the overall average marks of all students into an OUT parameter using SELECT INTO and display the retrieved average marks.
+-- 5.	Write a procedure that accepts a student ID as input and retrieves the course name of that student into an OUT parameter using SELECT INTO and display the retrieved course name
+
+
+-- sol 1
+delimiter //
+create procedure q1_2(out s_name varchar(30))
+begin
+select name into s_name 
+from students where marks >= all(select marks from students);
+select concat("Topper student of class is: ",s_name) as Highest_Scorer;
+end//
+
+call q1_2(@topper);
+
+select @topper as Topper_Name;
+-- -------------------------------------------------------------------------
+
+-- sol 2
+delimiter //
+create procedure q2_2(out s_name varchar(30))
+begin
+select name into s_name 
+from students where marks <= all(select marks from students);
+select concat("Lowest marks in class are scored by : ",s_name) as Lowest_Scorer;
+end//
+
+call q2_2(@lowestScore);
+
+select @lowestScore as Lowest_Scorer_Name;
+-- ---------------------------------------------------------------------------
+
+-- sol 3
+delimiter //
+create procedure q3_2(out s_no int)
+begin
+select count(*) into s_no
+from students; 
+select concat("Total count of Students is", s_no) as Total_Count;
+end//
+
+call q3_2(@Student_Count);
+
+select @Student_Count as Total_Student_Count;
+-- ------------------------------------------------------
+
+-- sol 4
+delimiter //
+create procedure q4_2(out avg_marks int)
+begin
+select avg(marks) into avg_marks
+from students;
+select concat("Average marks of total Students are: ", avg_marks) as Average_Marks;
+end//
+
+call q4_2(@Std_Avrg_Marks);
+
+select @Std_Avrg_Marks as Student_Average_Marks;
+-- ---------------------------------------------------------
+
+-- sol 5
+delimiter //
+create procedure q5_2(in s_id varchar(30), out c_name varchar(30))
+begin
+select course into c_name 
+from students where name = s_id;
+select concat("The Course name of ", s_id, " is: ", c_name) as c_det;
+end//
+
+call q5_2("Grace",@course_det);
+
+select @course_det as Student_course_details;
+
